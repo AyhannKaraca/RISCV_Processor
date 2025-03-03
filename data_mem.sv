a4a637979d2de1d3b81db554adba59102f722ded
Ayhan Karaca <84193287+AyhannKaraca@users.noreply.github.com>
2025-03-03 19:56:20 +0300

    Update data_mem.sv

--- data_mem.sv
+++ data_mem.sv
@@ -21,27 +21,20 @@
 
 
 module data_mem #(
-    parameter MEM_SIZE = 256
+    parameter MEM_SIZE = 16
 )(
-    input logic clk_i,
-    input logic reset_i,
-    
-    input logic MemWrite_i,
-    input logic MemRead_i,
-    input logic [31:0] Read_Address_i,
-    input logic [31:0] WriteData_i,
-    
+    input  logic        clk_i,
+    input  logic        reset_i,
+    input  logic        MemWrite_i,
+    input  logic        MemRead_i,
+    input  logic [31:0] Read_Address_i,
+    input  logic [31:0] WriteData_i,
     output logic [31:0] MemData_o 
     );
     
     logic [31:0] Data_Mem [0:MEM_SIZE-1];
-    
-    always_comb begin
-        if(MemRead_i)
-            MemData_o = Data_Mem[Read_Address_i>>2];
-            else
-            MemData_o = 32'b0;
-    end
+
+    assign MemData_o = (MemRead_i) ? Data_Mem[Read_Address_i >> 2] : 32'b0;
     
     always_ff @(posedge clk_i) begin        
         if(reset_i)begin
@@ -52,6 +45,5 @@ module data_mem #(
         else if(MemWrite_i) begin
             Data_Mem[Read_Address_i>>2] <= WriteData_i;
         end
-    end
-    
+    end    
 endmodule
